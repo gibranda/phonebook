551d2d086ba678d5d4e066d559ba6565cb2c6701
gibranda <gibranda94@gmail.com>
2017-10-27 16:41:25 +0700

    Prepare template and several view

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,17 @@
+<template>
+<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Vue JS Phonebook App</strong> by <a href="#">Gibranda</a>. Access Source code here
+        <a href="https://github.com/gibranda/phonebook">Github.com</a>
+      </p>
+      <p>
+        <a class="icon" href="https://github.com/gibranda/phonebook">
+            <i class="fa fa-github"></i>
+        </a>
+      </p>
+    </div>
+  </div>
+</footer>
+</template>
