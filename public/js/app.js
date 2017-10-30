5ac029c583113f2586736dd0eb60cdefe29ce72a
gibranda <gibranda94@gmail.com>
2017-10-30 14:31:09 +0700

    search function

--- public/js/app.js
+++ public/js/app.js
@@ -43230,16 +43230,34 @@ var Update = __webpack_require__(64);
             updateActive: '',
             lists: {},
             errors: {},
-            loading: false
+            loading: false,
+            searchQuery: '',
+            temp: ''
         };
     },
+
+
+    watch: {
+        searchQuery: function searchQuery() {
+            var _this = this;
+
+            if (this.searchQuery.length > 0) {
+                this.temp = this.lists.filter(function (item) {
+                    return item.name.toLowerCase().indexOf(_this.searchQuery.toLowerCase()) > -1;
+                });
+            } else {
+                this.temp = this.lists;
+            }
+        }
+    },
+
     mounted: function mounted() {
-        var _this = this;
+        var _this2 = this;
 
         axios.post('/getData').then(function (response) {
-            return _this.lists = response.data;
+            return _this2.lists = _this2.temp = response.data;
         }).catch(function (error) {
-            return _this.errors = error.response.data.errors;
+            return _this2.errors = error.response.data.errors;
         });
     },
 
@@ -43261,14 +43279,14 @@ var Update = __webpack_require__(64);
             this.updateActive = '';
         },
         del: function del(key, id) {
-            var _this2 = this;
+            var _this3 = this;
 
             if (confirm("Are you sure?")) {
                 this.loading = !this.loading;
                 axios.delete('/phonebook/' + id).then(function (response) {
-                    _this2.lists.splice(key, 1);_this2.loading = !_this2.loading;
+                    _this3.lists.splice(key, 1);_this3.loading = !_this3.loading;
                 }).catch(function (error) {
-                    return _this2.errors = error.response.data.errors;
+                    return _this3.errors = error.response.data.errors;
                 });
             }
         }
@@ -43779,9 +43797,35 @@ var render = function() {
               : _vm._e()
           ]),
           _vm._v(" "),
-          _vm._m(0),
+          _c("div", { staticClass: "panel-block" }, [
+            _c("p", { staticClass: "control has-icons-left" }, [
+              _c("input", {
+                directives: [
+                  {
+                    name: "model",
+                    rawName: "v-model",
+                    value: _vm.searchQuery,
+                    expression: "searchQuery"
+                  }
+                ],
+                staticClass: "input is-small",
+                attrs: { type: "text", placeholder: "search" },
+                domProps: { value: _vm.searchQuery },
+                on: {
+                  input: function($event) {
+                    if ($event.target.composing) {
+                      return
+                    }
+                    _vm.searchQuery = $event.target.value
+                  }
+                }
+              }),
+              _vm._v(" "),
+              _vm._m(0)
+            ])
+          ]),
           _vm._v(" "),
-          _vm._l(_vm.lists, function(item, key) {
+          _vm._l(_vm.temp, function(item, key) {
             return _c("a", { staticClass: "panel-block" }, [
               _c("span", { staticClass: "column is-9" }, [
                 _vm._v("\r\n        " + _vm._s(item.name) + "\r\n    ")
@@ -43851,17 +43895,8 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "panel-block" }, [
-      _c("p", { staticClass: "control has-icons-left" }, [
-        _c("input", {
-          staticClass: "input is-small",
-          attrs: { type: "text", placeholder: "search" }
-        }),
-        _vm._v(" "),
-        _c("span", { staticClass: "icon is-small is-left" }, [
-          _c("i", { staticClass: "fa fa-search" })
-        ])
-      ])
+    return _c("span", { staticClass: "icon is-small is-left" }, [
+      _c("i", { staticClass: "fa fa-search" })
     ])
   }
 ]
--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -16,14 +16,14 @@
 
   <div class="panel-block">
     <p class="control has-icons-left">
-      <input class="input is-small" type="text" placeholder="search">
+      <input class="input is-small" type="text" placeholder="search" v-model="searchQuery">
       <span class="icon is-small is-left">
         <i class="fa fa-search"></i>
       </span>
     </p>
   </div>
 
-  <a class="panel-block" v-for="item, key in lists">
+  <a class="panel-block" v-for="item, key in temp">
     <span class="column is-9">
         {{ item.name }}
     </span>
@@ -68,12 +68,27 @@ let Update = require('./Update.vue');
                 updateActive: '',
                 lists:{},
                 errors:{},
-                loading:false
+                loading:false,
+                searchQuery:'',
+                temp:''
             }
         },
+        
+        watch:{
+          searchQuery(){
+            if(this.searchQuery.length > 0) {
+              this.temp = this.lists.filter((item) => {
+                return item.name.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1
+              });
+            } else{
+              this.temp = this.lists
+            }
+          }
+        },
+
         mounted(){
           axios.post('/getData')
-          .then((response)=> this.lists = response.data)
+          .then((response)=> this.lists = this.temp = response.data)
                 .catch((error) => this.errors = error.response.data.errors)
         },
         methods:{
